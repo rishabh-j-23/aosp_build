5f4e6ba299a82a8f79887120e9c7782bbe748d01
Rishabh <99457863+rishabh-j-23@users.noreply.github.com>
2024-09-09 04:44:19 +0530

    Create build.sh

---
+++ build.sh
@@ -0,0 +1,8 @@
+https://github.com/PixelOS-Devices/device_xiaomi_ginkgo.git device/xiaomi/ginkgo && \
+https://github.com/PixelOS-Devices/kernel_xiaomi_ginkgo.git kernel/xiaomi/ginkgo && \
+https://github.com/PixelOS-Devices/vendor_xiaomi_ginkgo.git vendor/xiaomi/ginkgo && \
+
+/opt/crave/resync.sh && \
+source build/envsetup.sh && \
+lunch aosp_ginkgo-user && \
+mka bacon 
