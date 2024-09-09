59aec7f4994898de7a6cbd623a43bbed6a3bd147
Rishabh <99457863+rishabh-j-23@users.noreply.github.com>
2024-09-09 19:36:32 +0530

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,8 +1,14 @@
-git clone https://github.com/PixelOS-Devices/device_xiaomi_ginkgo.git device/xiaomi/ginkgo && \
-git clone https://github.com/PixelOS-Devices/kernel_xiaomi_ginkgo.git kernel/xiaomi/ginkgo && \
-git clone https://github.com/PixelOS-Devices/vendor_xiaomi_ginkgo.git vendor/xiaomi/ginkgo && \
-
+rm -rf .repo/local_manifests/ && \
+# Clone local_manifests repository
+repo init -u https://github.com/PixelOS-AOSP/manifest.git -b fourteen --git-lfs --depth=1 && \
+git clone https://github.com/rishabh-j-23/local_manifests --depth 1 -b main .repo/local_manifests && \
+# Sync the repositories
 /opt/crave/resync.sh && \
+# Set up build environment
+export BUILD_USERNAME=Ryxke && \
+export BUILD_HOSTNAME=crave && \
 source build/envsetup.sh && \
-lunch aosp_ginkgo-user && \
-mka bacon 
+# Build the ROM
+lunch aosp_ginkgo-ap2a-user && \
+make installclean && \
+mka bacon
