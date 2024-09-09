bb7e8017e36f78d9a0a553ccabc136a166092cf7
Rishabh <99457863+rishabh-j-23@users.noreply.github.com>
2024-09-09 07:47:51 +0530

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,6 +1,6 @@
-https://github.com/PixelOS-Devices/device_xiaomi_ginkgo.git device/xiaomi/ginkgo && \
-https://github.com/PixelOS-Devices/kernel_xiaomi_ginkgo.git kernel/xiaomi/ginkgo && \
-https://github.com/PixelOS-Devices/vendor_xiaomi_ginkgo.git vendor/xiaomi/ginkgo && \
+git clone https://github.com/PixelOS-Devices/device_xiaomi_ginkgo.git device/xiaomi/ginkgo && \
+git clone https://github.com/PixelOS-Devices/kernel_xiaomi_ginkgo.git kernel/xiaomi/ginkgo && \
+git clone https://github.com/PixelOS-Devices/vendor_xiaomi_ginkgo.git vendor/xiaomi/ginkgo && \
 
 /opt/crave/resync.sh && \
 source build/envsetup.sh && \
